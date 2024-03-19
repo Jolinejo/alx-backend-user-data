4ba24e0c13c489d965c985db4f92f95feb28fbd0
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-19 19:40:23 +0000

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,16 @@ class Auth:
     """ Authuntication class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ not used yet
+        """ returns True if the path is not in the list of excluded
         """
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
