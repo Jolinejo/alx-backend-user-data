f807086595868ef2f2c3d808ee8db04ad19f9bcd
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-19 19:27:34 +0000

    task

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def test_unauthorized():
     """ test unauthorized
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def test_forbid():
+    """ test forbid
+    """
+    abort(403)
