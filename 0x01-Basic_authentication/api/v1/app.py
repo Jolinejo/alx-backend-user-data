566e93d1f0cf41c81bbf64f0cf49100b682dd336
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-19 20:11:46 +0000

    trying

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,13 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +40,25 @@ def forbidden(error) -> str:
     """ Forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
+
+@app.before_request
+def filtering():
+    """ filtering of reqs
+    """
+    if auth is None:
+        pass
+
+    listi = ['/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/']
+    path_in = auth.require_auth(request.path, listi)
+    print(path_in)
+    if path_in is True:
+        pass
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
