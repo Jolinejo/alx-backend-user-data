f21bdd3238dee34f9ac330592cfb426c7d033823
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-20 14:01:21 +0000

    header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,20 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """returns the user email and password
+        from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        i = decoded_base64_authorization_header.find(':')
+        name = decoded_base64_authorization_header[:i]
+        password = decoded_base64_authorization_header[i+1:]
+        return (name, password)
