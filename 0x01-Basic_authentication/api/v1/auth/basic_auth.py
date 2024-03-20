d9a4408f02089dca17c97aaeff92e94db107802b
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-20 10:59:10 +0000

    header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,17 @@ from .auth import Auth
 class BasicAuth(Auth):
     """ C lass for basic Auth
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header
+        for a Basic Authentication:
+        """
+
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
