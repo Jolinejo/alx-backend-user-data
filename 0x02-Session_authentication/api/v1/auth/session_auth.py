9907ace7cd1b738f8bbc116f23922060090dfc2d
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-23 18:14:19 +0000

    task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ from flask import request
 from typing import List, TypeVar
 from .auth import Auth
 import base64
+import uuid
 from models.user import User
 
 
@@ -13,4 +14,16 @@ class SessionAuth(Auth):
     validate if everything inherits correctly without any overloading
     validate the “switch” by using environment variables
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
