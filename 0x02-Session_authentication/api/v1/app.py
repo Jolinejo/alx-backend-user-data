c54af45a02ece1fa7024d879f1d3becb0ec58852
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-23 18:46:06 +0000

    task

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,11 +38,13 @@ def filtering():
 
     path_in = auth.require_auth(request.path, ['/api/v1/status/',
                                                '/api/v1/unauthorized/',
-                                               '/api/v1/forbidden/'])
+                                               '/api/v1/forbidden/',
+                                               '/api/v1/auth_session/login/'])
     if path_in is False:
         return
     if auth.authorization_header(request) is None:
-        abort(401)
+        if auth.session_cookie(request) is None:
+            abort(401)
     request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
