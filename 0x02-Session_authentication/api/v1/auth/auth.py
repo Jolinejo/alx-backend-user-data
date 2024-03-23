584d841de838bbe820739c74c8d162cdb0de0720
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-23 18:40:24 +0000

    task

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -32,3 +33,12 @@ class Auth:
         """ not used yet
         """
         return None
+
+    def session_cookie(self, request=None):
+        """
+        returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
