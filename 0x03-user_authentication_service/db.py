81e3ce09540ec5cf2bdac5c29e36d93c11e44bb0
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-26 06:44:49 +0000

    first try

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,8 +35,11 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         save the user to the database
+        args:
+        email str
+        hashed_password str
         """
         user = User(email=email, hashed_password=hashed_password)
-        self.__session.add(user)
-        self.__session.commit()
+        self._session.add(user)
+        self._session.commit()
         return user
