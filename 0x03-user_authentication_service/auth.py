2c4735195fef1343a254940199d190e054d85c62
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-26 20:08:38 +0000

    18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,6 +92,20 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        update the user’s hashed_password field
+        with the new hashed password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed = _hash_password(password)
+            self._db.update_user(user_id=user.id,
+                                 hashed_password=hashed,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """
